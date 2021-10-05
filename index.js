7c08ba39cb075eb2280642e01bc09e1c781edea4
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2021-10-05 21:34:26 +0700

    build server

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get('/', (req, res) => res.json({ message: 'Hello World' }))
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
 })
